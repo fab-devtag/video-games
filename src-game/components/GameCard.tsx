783d5bfec239652d84ebea43c0ea6333bd235498
Fabien <fabien.sens@free.fr>
2023-03-31 15:23:49 +0200

    Displaying platform icons
    recheck pour l'interface et le desctructuring

--- src-game/components/GameCard.tsx
+++ src-game/components/GameCard.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Card, CardBody, Image, Heading } from '@chakra-ui/react';
+import { Card, CardBody, Image, Heading, Text } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
+import PlatformIconList from './PlatformIconList';
 
 interface Props {
 	game: Game;
 }
 
-const GameCard = ({ game: { name, background_image } }: Props) => {
+const GameCard = ({
+	game: { name, background_image, parent_platforms },
+}: Props) => {
 	return (
 		<Card borderRadius={10} overflow="hidden">
 			<Image src={background_image} />
 			<CardBody>
 				<Heading fontSize="2xl">{name}</Heading>
+				<PlatformIconList
+					platforms={parent_platforms.map(({ platform }) => platform)}
+				/>
 			</CardBody>
 		</Card>
 	);
